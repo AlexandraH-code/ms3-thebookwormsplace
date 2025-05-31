ecdc0646df1dd5cf7605172013b5dace1e4171c4
AlexandraH-code <alexandra_holstensson@hotmail.com>
2025-05-31 10:05:36 +0200

    Add url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path  # import path, similar to project's urls.py
 from . import views  # import views.py from the current directory
-
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.home, name='home'),
+    path('books/', views.books, name='books'),
+    path('books/<int:pk>', views.book_detail, name='book_detail'),
+    path('about/', views.about, name='about'),
+    path('register/', views.register, name='register'),
+    path('login', views.login_view, name='login'),
+    path('logout', LogoutView.as_view(next_page='home'), name='logout')
 ]
