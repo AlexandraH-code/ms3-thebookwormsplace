524b65acc617d3bcf780d5c217fabd715195a519
AlexandraH-code <alexandra_holstensson@hotmail.com>
2025-06-02 15:58:46 +0200

    Show images on book-page

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class BlogPost(models.Model):
     slug = models.SlugField(max_length=200)
     author = models.CharField(max_length=100)
     cover_image = models.ImageField(upload_to='book_covers/')
+    # cover_image = models.ImageField(upload_to='book_covers/', blank=True, null=True)
     # cover_image = CloudinaryField('image', default='placeholder')
     cover_image_alt = models.CharField(max_length=150, null=False, blank=False)
     description = models.TextField()
@@ -26,8 +27,7 @@ class BlogPost(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    blogpost = models.ForeignKey(BlogPost, on_delete=models.CASCADE, 
-                                 related_name='comments')
+    book = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -37,7 +37,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s comment"
-
+    
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- thebookwormsplace/urls.py
+++ thebookwormsplace/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # from blog import views as blog_views
 # from blog import urls as blog_urls
 
@@ -28,3 +30,6 @@ urlpatterns = [
     path('', include('blog.urls'), name='blog_urls'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
